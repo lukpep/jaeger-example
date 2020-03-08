f51ce20d5fb76c7eb6caecf99e78453a1659fc83
Łukasz Pepliński <lukpep@gmail.com>
2020-03-08 11:50:03 +0100

    changed example domain to a little more interesting

--- src/ConsoleApp_Example1/Class1.cs
+++ src/ConsoleApp_Example1/Class1.cs
@@ -1,45 +1,72 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
 using OpenTracing;
 
 namespace ConsoleApp_Example1
 {
-    internal class Hello
+    internal class NsaDataAccessor
     {
         private readonly ITracer _tracer;
-        private readonly ILogger<Hello> _logger;
+        private readonly ILogger<NsaDataAccessor> _logger;
 
-        public Hello(ITracer tracer, ILoggerFactory loggerFactory)
+        private NsaDataAccessor(ITracer tracer, ILoggerFactory loggerFactory)
         {
             _tracer = tracer;
-            _logger = loggerFactory.CreateLogger<Hello>();
+            _logger = loggerFactory.CreateLogger<NsaDataAccessor>();
         }
 
-        public void SayHello(string helloTo)
+        private async Task<string> PrepareSecretData()
         {
-            var span = _tracer.BuildSpan("say-hello").Start();
-            span.SetTag("hello-to", helloTo);
-            var helloString = $"Hello, {helloTo}!";
-            span.Log(new Dictionary<string, object>
-                {
-                    [LogFields.Event] = "string.Format",
-                    ["value"] = helloString
-                }
-            );
-            _logger.LogInformation(helloString);
-            span.Log("WriteLine");
-            span.Finish();
-        }
-
-        public static void Main(string[] args)
-        {
-            using (var loggerFactory = new LoggerFactory())
+            using (var scope = _tracer.BuildSpan("prepare-classified-data").StartActive(true))
             {
-                var helloTo = "lpe";
-                using (var tracer = Tracing.Init("hello-world", loggerFactory))
+                const string helloString = "Hello, this is totally not any secret data!";
+                await SomeLongInternalFunction();
+                scope.Span.Log(new Dictionary<string, object>
                 {
-                    new Hello(tracer, loggerFactory).SayHello(helloTo);
+                    [LogFields.Event] = "contacting NSA for ... something",
+                    ["value"] = helloString
+                });
+                return helloString;
+            }
+        }
+
+        private async Task SomeLongInternalFunction()
+        {
+            using (var scope = _tracer.BuildSpan("getting-data-from-secret-database").StartActive(true))
+            {
+                scope.Span.Log("getting it");
+                await Task.Delay(10);
+            }
+        }
+
+        private void PrintData(string helloString)
+        {
+            using (var scope = _tracer.BuildSpan("sending-data-to-satellite").StartActive(true))
+            {
+                _logger.LogInformation(helloString);
+                scope.Span.Log("Data send");
+            }
+        }
+
+        private async Task GetData()
+        {
+            using (var scope = _tracer.BuildSpan("show-secret-data").StartActive(true))
+            {
+                var data = await PrepareSecretData();
+                PrintData(data);
+            }
+        }
+
+        public static async Task Main(string[] args)
+        {
+            var a = new LoggerFactory();
+            
+            using (var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole()))
+            {
+                using (var tracer = Tracing.Init("secret-data-service", loggerFactory))
+                {
+                    await new NsaDataAccessor(tracer, loggerFactory).GetData();
                 }
             }
         }
